a84f7d3a3d3516d2e4682cd745842eff380a041d
Ondrej Hryzak <ondrej@blindshell.com>
2024-09-09 18:47:28 +0200

    hw6

--- lesson_4/homework.py
+++ lesson_4/homework.py
@@ -1,40 +1,21 @@
+from light import Light, led_map, WHITE_COLOR  # Import WHITE_COLOR here
 from neopixel import NeoPixel
 from microbit import pin0
 from microbit import sleep
 
-WHITE_COLOR = (255, 255, 255)
-OFF_COLOR = (0, 0, 0)
+class Main:
+    def __init__(self):
+        self.np = NeoPixel(pin0, 8)
+        self.light_left_front = Light(led_map["LED_FRONT_LEFT_IN"], WHITE_COLOR, self.np)
 
-led_map = {
-    "LED_FRONT_LEFT_IN": 0,
-    "LED_FRONT_LEFT_OUT": 1,
-    "LED_FRONT_RIGHT_OUT": 2,
-    "LED_FRONT_RIGHT_IN": 3,
-    "LED_REAR_RIGHT_IN": 4,
-    "LED_REAR_RIGHT_OUT": 5,
-    "LED_REAR_LEFT_OUT": 6,
-    "LED_REAR_LEFT_IN": 7
-}
+    def run(self):
+        while True:
+            self.light_left_front.start_led()
+            sleep(1000)
+            self.light_left_front.stop_led()
+            sleep(1000)
+        
 
-np = NeoPixel(pin0, 8)
-led_index = led_map.get("LED_REAR_RIGHT_OUT")
-
-def start_led(led_index):
-    set_led_color(led_index, WHITE_COLOR)
-
-def stop_led(led_index):
-    set_led_color(led_index, OFF_COLOR)
-
-def set_led_color(led_index, color):
-    np[led_index] = color
-    np.write()
-
-while True:
-    if led_index is not None:
-        start_led(led_index)
-        sleep(1000)
-        stop_led(led_index)
-        sleep(1000)
-    else:
-        print("Invalid LED name")
-
+if __name__ == "__main__":
+    main = Main()
+    main.run()
--- lesson_6/homework_pokrocily_part2.py
+++ lesson_6/homework_pokrocily_part2.py
@@ -3,28 +3,29 @@ from microbit import pin14
 from microbit import pin15
 from microbit import button_a
 
-tiky = 0  # globalni promena
+ticks = 0  # globalni promena
 
-def enkoder_signal(jmeno_enkoderu):
-    if jmeno_enkoderu == "pravy_enkoder":
+def encoder_signal(encoder_name):
+    if encoder_name == "right_encoder":
         return int(pin15.read_digital())
-    elif jmeno_enkoderu == "levy_enkoder":
+    elif encoder_name == "left_encoder":
         return int(pin14.read_digital())
     else:
         print("Zadali jste nepodporovane jmeno")
         return -1
 
-def pocet_tiku(jmeno_enkoderu):
-    # TODO volejte funkce enkoder_signal a pocitejte nove tiky
+def tick_count(encoder_name):
+    int new_tick = encoder_signal(encoder_name);
+    ticks = ticks + new_tick 
 
-    return tiky
+    return ticks
 
 if __name__ == "__main__":
 
    # misto vypisu vidi/nevidi vypisujte tiky
     while not button_a.was_pressed():
-        data_enkoderu = enkoder_signal("levy_enkoder")
-        if data_enkoderu == 1:
+        data_encoder = encoder_signal("left_encoder")
+        if data_encoder == 1:
             print("levy enkoder vidi")
         elif data_enkoderu == 0:
             print("levy enkoder nevidi")

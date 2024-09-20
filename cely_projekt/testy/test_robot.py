e40a02f4e233b917776000bda954282861c1b611
Ondrej Hryzak <ondrej@blindshell.com>
2024-09-20 14:52:27 +0200

    kalibrace motoru

--- cely_projekt/testy/test_robot.py
+++ cely_projekt/testy/test_robot.py
@@ -1,35 +1,64 @@
-from microbit import button_a, sleep, i2c
-
+from microbit import button_a, sleep, i2c, pin8, pin12, time_pulse_us
 from cely_projekt import Konstanty, KalibracniFaktory, Robot
 
-def zakladni_test_spusteni():
+class Ultrazvuk:
 
-    min_rychlost = 2.1658lu
-    min_pwm_rozjezd = 79 # kalibrace vytiskne na konci pri "zrychluj"
-    min_pwm_dojezd = 41 # kalibrace vytiskne na konci pri "zpomaluj"
-    a = 24.3732783404646 # ziskej z excelu
-    b = 8.21172006498485 # ziskej z excelu
+    def __init__(self):
+        self.__trigger = pin8
+        self.__echo = pin12
+
+        self.__trigger.write_digital(0)
+        self.__echo.read_digital()
+
+    def zmer_vzdalenost(self):
+
+        rychlost_zvuku = 340  # m/s
+
+        self.__trigger.write_digital(1)
+        self.__trigger.write_digital(0)
+
+        zmereny_cas_us = time_pulse_us(self.__echo, 1)
+        if zmereny_cas_us < 0:
+            return -1  # Return -1 if measurement failed
+
+        zmereny_cas_s = zmereny_cas_us / 1000000
+        vzdalenost = zmereny_cas_s * rychlost_zvuku / 2
+
+        return vzdalenost
+
+def start_robot(ult):
+
+    min_rychlost = 0.31
+    min_pwm_rozjezd = 47  # kalibrace vytiskne na konci pri "zrychluj"
+    min_pwm_dojezd = 35   # kalibrace vytiskne na konci pri "zpomaluj"
+    a = 15.22801354       # ziskej z excelu
+    b = 42.27615142       # ziskej z excelu
 
     levy_faktor = KalibracniFaktory(min_rychlost, min_pwm_rozjezd, min_pwm_dojezd, a, b)
 
-    min_rychlost = 2.171571
-    min_pwm_rozjezd = 113
-    min_pwm_dojezd = 113
-    a = 27.4515630414309
-    b = 61.3869817945568
+    min_rychlost = 0.7
+    min_pwm_rozjezd = 39
+    min_pwm_dojezd = 28
+    a = 17.05925837
+    b = 27.05851914
 
     pravy_faktor = KalibracniFaktory(min_rychlost, min_pwm_rozjezd, min_pwm_dojezd, a, b)
 
     robot = Robot(0.15, 0.067, levy_faktor, pravy_faktor, True)
     robot.inicializuj()
-    robot.jed(0.067*Konstanty.PI, 0)
+    robot.jed(0.067 * Konstanty.PI, 0)  # Start moving forward
 
     while not button_a.was_pressed():
+        vzdalenost = ult.zmer_vzdalenost()
+        if vzdalenost > 0 and vzdalenost <= 0.3:
+            robot.jed(0, 0)  # Stop the robot
+            break
         sleep(5)
         robot.aktualizuj_se()
 
-    robot.jed(0,0)
+    robot.jed(0, 0)  # Ensure the robot is stopped when exiting the loop
 
-if __name__ =="__main__":
-    zakladni_test_spusteni()
+if __name__ == "__main__":
+    ult = Ultrazvuk()
+    start_robot(ult)
 
